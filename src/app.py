d7bec9b1b59117f623575c034b0ba9d3575ee402
Pratik Karangale <123284598+Soldier480@users.noreply.github.com>
2026-03-23 17:21:51 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import os
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
